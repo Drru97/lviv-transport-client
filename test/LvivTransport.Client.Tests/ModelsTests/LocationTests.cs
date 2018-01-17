0020410d694376c4a099c7b6d14a4a3579dd1200
Drru97 <oleg.dystak@gmail.com>
2018-01-17 15:30:01 +0200

    Add unit tests for models.

---
+++ test/LvivTransport.Client.Tests/ModelsTests/LocationTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using LvivTransport.Client.Core.Models;
+using Xunit;
+
+namespace LvivTransport.Client.Tests.ModelsTests
+{
+    public class LocationTests
+    {
+        [Fact]
+        public void Location_LatitudeGreater90_ExceptionThrown()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => CreateLocation(90.0003, 0));
+        }
+
+        [Fact]
+        public void Location_LatitudeLessMinus90_ExceptionThrown()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => CreateLocation(-92.3, 0));
+        }
+
+        [Fact]
+        public void Location_LongitudeGreater180_ExceptionThrown()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => CreateLocation(0, 180.2));
+        }
+
+        [Fact]
+        public void Location_LongitudeLessMinus180_ExceptionThrown()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => CreateLocation(0, -181.7));
+        }
+
+        private static Location CreateLocation(double latitude, double longitude)
+        {
+            return new Location(latitude, longitude);
+        }
+    }
+}
---
+++ test/LvivTransport.Client.Tests/RequestsTests/RoutesRequestTests.cs
@@ -0,0 +1,29 @@
+﻿using LvivTransport.Client.Core.Request;
+using Xunit;
+
+namespace LvivTransport.Client.Tests.RequestsTests
+{
+    public class RoutesRequestTests
+    {
+        [Fact]
+        public void RoutesRequest_ParameterNotSet_ReturnsNoParameter()
+        {
+            var request = CreateRoutesRequest();
+
+            Assert.False(request.HasParameter);
+        }
+
+        [Fact]
+        public void RoutesRequest_ParameterSet_ReturnsHasParameter()
+        {
+            var request = CreateRoutesRequest(int.MaxValue);
+
+            Assert.True(request.HasParameter);
+        }
+
+        private static RoutesRequest CreateRoutesRequest(int? param = null)
+        {
+            return new RoutesRequest(param);
+        }
+    }
+}
